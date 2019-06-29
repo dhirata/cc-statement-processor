d0b96f98dfca9e9de640c52c1e68639dc5b9f036
Derek Hirata <hirataderek@gmail.com>
2019-06-29 13:13:05 -0400

    Small configuration changes

--- cc_statement_processor/parser/pdfparser.py
+++ cc_statement_processor/parser/pdfparser.py
@@ -1,4 +1,4 @@
 from tabula import convert_into
 
 def parse_pdf(path):
-	convert_into(path, "test.csv", output_format="csv")
+	convert_into(path, "test.csv", output_format="csv", pages="3,4", guess=False)
