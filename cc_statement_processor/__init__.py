009edb410af616eec87d5a2797774b244f45c188
Derek Hirata <hirataderek@gmail.com>
2019-06-30 22:53:20 -0400

    Adding validations for path

--- cc_statement_processor/__init__.py
+++ cc_statement_processor/__init__.py
@@ -1,7 +1,15 @@
+import PyPDF2
 import sys
 from parser.pdfparser import parse_statement_pdf
 
 def main():
 	# put main stuff here
-	parse_statement_pdf(sys.argv[1])
+	path_to_pdf = sys.argv[1]
+	try:
+		# Validations 
+		PyPDF2.PdfFileReader(open(path_to_pdf, "rb"))
+		print("PDF file valid")
+	except:
+		raise ValueError("Error: Invalid PDF file")
+	parse_statement_pdf(path_to_pdf)
 
